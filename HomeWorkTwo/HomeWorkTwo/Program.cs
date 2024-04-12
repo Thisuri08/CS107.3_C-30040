1a9afe0af11cdcc5bc2e6189fea42003396ca7cb
Thisuri08 <137777879+Thisuri08@users.noreply.github.com>
2024-04-13 00:05:58 +0530

    Add files via upload

---
+++ HomeWorkTwo/HomeWorkTwo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validNumber;
+            do {
+                Console.WriteLine("Enter your age?");
+                string age = Console.ReadLine();
+
+                validNumber = int.TryParse(age, out int Age);
+
+                if (validNumber)
+                {
+                    Console.WriteLine($"Your age is {Age}");
+                    Console.WriteLine($"You will be {Age + 10} in ten years");
+                    Console.WriteLine($"You were {Age - 10} years old ten years ago ");
+                }
+                else
+                {
+                    Console.WriteLine( "You entered an invalid number, try again.");
+
+
+                }
+            } while (!validNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
