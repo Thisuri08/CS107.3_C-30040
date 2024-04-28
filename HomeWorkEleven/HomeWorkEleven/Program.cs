fd2bf8b86da1bea43d86ca7c080ede8440a00cbc
Thisuri08 <137777879+Thisuri08@users.noreply.github.com>
2024-04-28 20:25:40 +0530

    Add files via upload

---
+++ HomeWorkEleven/HomeWorkEleven/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkEleven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WellcomeMsg();
+
+           string name = UserName();
+
+            SayHi(name);
+
+            Console.ReadLine();
+
+        }
+
+        public static void WellcomeMsg()
+        {
+            Console.WriteLine("Wellcome to our application!");
+        }
+
+        public static string UserName()
+        {
+            Console.WriteLine("Enter your first name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        public static void SayHi(string name) {
+
+            Console.WriteLine($"Hello {name}");
+
+        }
+    }
+}
