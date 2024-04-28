cb4872aa4414aa91fd81fc0bd6f0ba9e8d622934
Thisuri08 <137777879+Thisuri08@users.noreply.github.com>
2024-04-28 20:23:47 +0530

    Add files via upload

---
+++ HomeWorkNine/HomeWorkNine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkNine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the list of first names in a row: ");
+            string names = Console.ReadLine();
+
+            Console.WriteLine();
+
+            string[] firstNames = names.Split(',');
+
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                Console.WriteLine($"hello {firstNames[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
