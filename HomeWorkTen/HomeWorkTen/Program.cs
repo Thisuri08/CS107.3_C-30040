31596111c08e13eeec927dc1e76649ff2e765d7e
Thisuri08 <137777879+Thisuri08@users.noreply.github.com>
2024-04-28 20:24:29 +0530

    Add files via upload

---
+++ HomeWorkTen/HomeWorkTen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkTen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstNames = new List<string>();
+            string userAnswer;
+
+            do
+            {
+                Console.WriteLine("Enter ypur first name? ");
+                string name = Console.ReadLine();
+
+                firstNames.Add(name);
+
+                Console.WriteLine("Do you want to keep adding names? ");
+                userAnswer = Console.ReadLine();
+
+                if (userAnswer.ToLower() == "no")
+                {
+                    Console.WriteLine($"you've already added {firstNames.Count} number of people to the list. ");
+                    Console.WriteLine();
+
+                    foreach (string fname in firstNames)
+                    {
+                        Console.WriteLine($"Hello {fname}");
+                    }
+                }
+            } while (userAnswer.ToLower() == "yes");
+
+            Console.ReadLine();
+        }
+    }
+}
